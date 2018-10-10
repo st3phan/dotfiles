896369bc66ac0342fd73bb859f469cb7386c8364
Stephan van Opstal <stephan@vanopstal.eu>
2018-10-10 20:32:51 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,20 +1,83 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Setting up your Mac..."
+echo "Preparing world domination..."
 
-# Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Include Adam Eivy's library helpers
+source ./lib.sh
+
+# Prevent sleep while install is running
+caffeinate &
+
+#####
+# Introduction
+#####
+
+fullname=$(osascript -e "long user name of (system info)")
+
+bot "Hi $fullname. I'm going to make your OSX system better. We're going to:"
+action "install Xcode's command line tools"
+action "install Homebrew and brew cask"
+action "if you feel like it, we will also install more things"
+
+bot "One more thing: I'll need your password from time to time."
+
+# ask for password upfront
+sudo -v
+
+read -r -p "Let's go? [y|N] " response
+if [[ $response =~ ^(y|yes|Y) ]];then
+    ok
+else
+    exit -1;
 fi
 
-# Update Homebrew recipes
+#####
+# install Xcode command line tools
+#####
+
+running "checking Xcode CLI install"
+xcode_select="xcode-select --print-path"
+xcode_install=$($xcode_select) 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
+    bot "You are missing the Xcode CLI tools. I'll launch the install for you, but then you'll have to restart the process again."
+    running "After that you'll need to paste the command and press Enter again."
+
+    read -r -p "Let's go? [y|N] " response
+    if [[ $response =~ ^(y|yes|Y) ]];then
+        xcode-select --install
+    fi
+
+    exit -1
+fi
+ok
+
+#####
+# install homebrew
+#####
+
+running "checking homebrew install"
+brew_bin=$(which brew) 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
+    action "installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [[ $? != 0 ]]; then
+        error "unable to install homebrew, script $0 abort!"
+        exit -1
+    fi
+fi
+ok
+
+running "checking brew-cask install"
+output=$(brew tap | grep cask)
+if [[ $? != 0 ]]; then
+    action "installing brew-cask"
+    require_brew caskroom/cask/brew-cask
+fi
+ok
+
+# Make sure we’re using the latest Homebrew
+running "updating homebrew"
 brew update
-
-# Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle
-
-# Get dotfiles installation directory
-#DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-#ln -sf "$DOTFILES_DIR/.gitconfig" ~
+brew tap homebrew/versions
+brew tap caskroom/versions
+ok
