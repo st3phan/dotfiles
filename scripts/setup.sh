b120b3ea42269e0456c361bbe6fd4427a8c512e8
Stephan van Opstal <stephan@vanopstal.eu>
2018-10-30 21:24:48 +0100

    Update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -23,7 +23,7 @@ ask_details() {
     clear
         bold_echo 'Your Mac App Store details to install apps:'
         read -p 'MAS email: ' mas_email
-        read -s -p 'MAS password (will not be echoed): ' mas_password
+        read -p 'MAS password (will not be echoed): ' mas_password
 
     clear
         bold_echo 'Some details to be used when configuring git:'
